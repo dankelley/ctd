e868b2332deaceb4d61eb8d297fbc851b804f2b1
dankelley <kelley.dan@gmail.com>
2023-04-27 09:37:38 -0300

    add path argument to getDatabaseName()

--- R/database.R
+++ R/database.R
@@ -4,7 +4,7 @@
 #' where the upper-case items are inferred from corresponding parameters.
 #'
 #' @param prefix character value indicating the prefix for the database
-#' name.
+#' name.  Often, this will be the name of a project, or application.
 #'
 #' @param username character value for the user's login name. If not provided,
 #' an attempt is made to construct this using [Sys.info()].
@@ -12,20 +12,29 @@
 #' @param suffix optional character value that may be included in the filename,
 #' if provided.
 #'
+#' @param path character value indicating the directory to contain the file. This
+#' defaults to the present directory.
+#'
 #' @return [getDatabaseName] returns a character value constructed by
 #' stringing `prefix`, `username` (and possibly `suffix`) together, separated
 #' by underscore characters, with `.db` being added to the end;
 #' see \sQuote{Examples}.
 #'
 #' @examples
-#' getDatabaseName() # ctdTag_kelley.db, for the author
+#' getDatabaseName() # ./kelley.db, for the author
+#' getDatabaseName(path="/Users/kelley/databases") # /Users/kelley/databases/kelley.db, for the author
+#' getDatabaseName("foo") # ./foo_kelley.db, for the author
+#' getDatabaseName("foo", "bar") # foo_bar.db
 #' getDatabaseName("foo", "bar", "01") # foo_bar_01.db
 #'
 #' @author Dan Kelley
 #'
 #' @export
-getDatabaseName <- function(prefix="ctdTag", username, suffix)
+getDatabaseName <- function(prefix, username, suffix, path=".")
 {
+    res <- paste0(path, "/")
+    if (!missing(prefix))
+        res <- paste0(res, prefix)
     if (missing(username)) {
         # FIXME: not sure which of the following is best for general computers.
         # As seems to often be the case, Windows can be problematic.
@@ -34,9 +43,10 @@ getDatabaseName <- function(prefix="ctdTag", username, suffix)
         if (is.null(username) || 0L == nchar(username))
             username <- "unknown"
     }
-    if (missing(suffix))
-        file.path(paste0(prefix, "_", username, ".db"))
-    else
-        file.path(paste0(prefix, "_", username, "_", suffix, ".db"))
+    res <- paste0(res, if (!missing(prefix)) "_" else "", username)
+    if (!missing(suffix))
+        res <- paste0(res, "_", suffix)
+    res <- paste0(res, ".db")
+    file.path(res)
 }
 
--- tests/testthat/test-ctdTag.R
+++
@@ -1,3 +0,0 @@
-test_that("getDatabaseName() works", {
-    expect_equal("foo_bar_01.db", getDatabaseName("foo", "bar", "01"))
-})
